e4df3b977436988e02a2f2fff6e914dd20ba9462
Musa Demir <alpermusademir@gmail.com>
2022-12-19 00:14:55 +0300

    anasayfa düzenlendi.

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,7 +6,6 @@ import {getAllProjects} from "../../functions/getAllProjects";
 import ProjectCard from "../../components/ProjectCard";
 
 export default function ProjectPages({projects}) {
-    console.log(projects)
     return (
         <Layout title={"Portfolio / Projects"} description={"My Projects"}>
             <section className="h-full max-w-5xl pt-4 mx-auto mb-16 md:pt-16">
